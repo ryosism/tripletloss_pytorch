109ca3b05d24f1780298e1b5080aa614aca31f6d
Ryo Sobue <sn0wbreeze2000@yahoo.co.jp>
2019-11-29 14:59:14 +0900

    実験条件の変更，ここで指定するパラメータの追加

--- paramConfig.py
+++ paramConfig.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 
-NUM_TRAINEX = "18"
+NUM_TRAINEX = "21"
 EPOCH = 500
 OPTIMIZER = 'Adam'
 LOG_INTERVAL = 200
-BATCH_SIZE = 1
+BATCH_SIZE = 4
 GRAPH_PDF = "./ex{}/train_loss.pdf".format(NUM_TRAINEX)
 TSNE_DEBUG = False
 LOSS_MARGIN = 1.0
+IMG_SIZE = (384, 512)
+NET_DIMENTIONS = 64
 
-NUM_TESTEX = "11"
+NUM_TESTEX = "19"
 PARAM_EPOCH = "96"
 TRAINED_PARAM = "./ex{}/params_ex{}_epoch{}.ckpt".format(NUM_TESTEX, NUM_TESTEX, PARAM_EPOCH)
-TEST_DIR_ROOT = "/home/dataset/recipeVideoDataset/"
+TEST_DIR_ROOT = "/home/dataset/recipeVideoDataset/video_and_frames/"
 TEST_BATCH_SIZE = 24
 LOGFILE = './log/test_log_ex{}.log'.format(NUM_TESTEX)
+TEST_NET_DIMENTIONS = 128
+TEST_IMG_SIZE=(768, 1024)
